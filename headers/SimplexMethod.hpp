8cab0c9d99291d0c8fa377f32b6d3a59e0b3c513
Mistiurin Ilia <i.mistiurin@innopolis.university>
2023-09-17 21:51:21 +0300

    Initial templates for Simplex Methods

--- headers/SimplexMethod.hpp
+++ headers/SimplexMethod.hpp
@@ -6,9 +6,37 @@
 #define OPTIMIZATION_COURSE_ASSIGNMENT_1_SIMPLEXMETHOD_HPP
 
 
+#include "Vector.hpp"
+#include "Matrix.hpp"
+
+using namespace std;
+
 class SimplexMethod {
+public:
+
+
+private:
+
+    int define_pivot_col(Vector &net_eval);
+
+    Vector calculate_ratio(Matrix &main_matrix, Vector &pivot_col);
+
+    float define_pivot_row(Vector &ratio);
+
+    float define_pivot_element(Matrix &main_matrix, Vector &pivot_col, Vector &pivot_row);
+
+    Vector define_basis(Vector &basis, Vector &pivot_row, Vector &piwvt_col, Vector &function_coefficients);
+
+    Matrix update_main_matrix(Matrix &main_matrix, Vector &pivot_row, Vector &pivot_col, float pivot_element);
+
+    Vector define_basis_element(Vector &basis, Vector &pivot_row, Vector &pivot_col);
+
+    Vector calculate_profit(Matrix &main_matrix, Vector &basis);
+
+    Vector calculate_net_evaluation(Vector &function_coefficients, Vector &profit);
+
+    bool check_net_evaluation(Vector &net_eval);
 
 };
-
 
 #endif //OPTIMIZATION_COURSE_ASSIGNMENT_1_SIMPLEXMETHOD_HPP
--- source/SimplexMethod.cpp
+++ source/SimplexMethod.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "../headers/SimplexMethod.hpp"
+
+
+int SimplexMethod::define_pivot_col(Vector &net_eval) {
+    return 0;
+}
+
+Vector SimplexMethod::calculate_ratio(Matrix &main_matrix, Vector &pivot_col) {
+    return Vector();
+}
+
+float SimplexMethod::define_pivot_row(Vector &ratio) {
+    return 0;
+}
+
+float SimplexMethod::define_pivot_element(Matrix &main_matrix, Vector &pivot_col, Vector &pivot_row) {
+    return 0;
+}
+
+Vector SimplexMethod::define_basis(Vector &basis, Vector &pivot_row, Vector &piwvt_col, Vector &function_coefficients) {
+    return Vector();
+}
+
+Matrix
+SimplexMethod::update_main_matrix(Matrix &main_matrix, Vector &pivot_row, Vector &pivot_col, float pivot_element) {
+    return Matrix();
+}
+
+Vector SimplexMethod::define_basis_element(Vector &basis, Vector &pivot_row, Vector &pivot_col) {
+    return Vector();
+}
+
+Vector SimplexMethod::calculate_profit(Matrix &main_matrix, Vector &basis) {
+    return Vector();
+}
+
+Vector SimplexMethod::calculate_net_evaluation(Vector &function_coefficients, Vector &profit) {
+    return Vector();
+}
+
+bool SimplexMethod::check_net_evaluation(Vector &net_eval) {
+    return false;
+}
