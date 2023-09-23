3623a98d0aee13b8a4d63d0cf60940c0420f30f4
Mistiurin Ilia <i.mistiurin@innopolis.university>
2023-09-23 12:14:45 +0300

    Main: added call of Simplex and data input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 
 // Custom classes
 #include "headers/Matrix.hpp"
@@ -8,8 +8,21 @@
 
 using namespace std;
 
+void check_data_1() {
+
+    Vector b(3), C(3);
+    Matrix A(3, 6);
+    float accuracy;
+
+    cin >> C >> A >> b >> accuracy;
+
+    SimplexMethod::start_simplex(A, b, C, accuracy);
+}
+
+
 int main() {
 
-    Matrix main_matrix(3, 7);
+    check_data_1();
+
     return EXIT_SUCCESS;
 }
